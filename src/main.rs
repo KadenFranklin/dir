54af74017a1872faf697e4d56d3d4485e0fab2a2
KadenFranklin <43596236+KadenFranklin@users.noreply.github.com>
2021-02-10 21:37:08 -0600

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,9 @@
-use std::env;
-use std:: fs::File;
-use std::io::{BufRead, BufReader, Result};
+use std::fs;
 
-fn main() -> io::Result<()> {
-    for arg in args().skip(1) {
-        let input_file = File::open(arg)?;
-        let reader = BufReader::new(input_file);
-        for line in reader.lines() {
-            println!("{}", line?);
-        }
+fn main() -> std::io::Result<()> {
+    for entry in fs::read_dir(".")? {
+        let dir = entry?;
+        println!("{:?}", dir.path());
     }
-    return Ok(())
+    Ok(())
 }
